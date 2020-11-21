e1c1d2527209a0266656f36e475e78b2ab1122d3
Shubham Pratap Singh <shubham-thakur@live.com>
2020-11-21 11:35:49 +0000

    cats are ruling the page now

--- cats/cat.js
+++
@@ -1,9 +0,0 @@
-function generator() {
-      fetch('https://aws.random.cat/meow')
-      .then(response => response.json())
-      .then(file => {
-      console.log(file);
-      file.replace(/\\\//g, "/");
-      console.log(file);
-      img.src=file;
-      });
